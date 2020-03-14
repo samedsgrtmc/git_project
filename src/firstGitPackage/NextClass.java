4277e56c015b0483bbda3ae4da05109341cb3807
samed <samedsgrtmc@gmail.com>
2020-03-15 00:00:48 +0300

    last message

--- src/firstGitPackage/NextClass.java
+++ src/firstGitPackage/NextClass.java
@@ -6,11 +6,10 @@ public class NextClass {
 
 		System.out.println("Hello git users");
 		System.out.println("Hello world!");
-<<<<<<< HEAD
-		System.out.println("Commmit 7");
-=======
+
+
 		System.out.println("Hello edit");
->>>>>>> 4b9d2cb3d77c1b30ba3177673bd1ca881922984e
+
 
 	}
 
